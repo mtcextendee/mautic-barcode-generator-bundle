8e3556e7e18c40ff855ec15f59b7c69d35a4eb4c
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-02-10 11:26:51 +0100

    Init commit

---
+++ MauticBarcodeGeneratorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MauticPlugin\MauticBarcodeGeneratorBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticBarcodeGeneratorBundle extends PluginBundleBase
+{
+}
