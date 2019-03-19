42950052fc618698ae85b6c5087411ddedec98d4
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-03-19 22:53:05 +0100

    Fix namespace

--- Token/TokenReplacer.php
+++ Token/TokenReplacer.php
@@ -9,7 +9,6 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-namespace Mautic\CoreBundle\Token;
 namespace MauticPlugin\MauticBarcodeGeneratorBundle\Token;
 
 abstract class TokenReplacer
